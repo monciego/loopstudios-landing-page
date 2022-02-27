02fd931b993e6f27e7d2177f42e7e90b32202533
Jericho <jerichobantiquete@gmail.com>
2022-02-27 22:21:06 +0800

    Import images and created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./header.css";
+import logo from "../../images/logo.svg";
+import menu from "../../images/icon-hamburger.svg";
+import close from "../../images/icon-close.svg";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleHandler = () => {
+    setToggle((prevState) => !prevState);
+  };
+
+  return (
+    <header>
+      <nav>
+        <div className="container">
+          <div className="logo">
+            <a href="#0">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+
+          <ul className={toggle ? "active" : ""}>
+            <li>
+              <a href="#0">About</a>
+            </li>
+            <li>
+              <a href="#0">Careers</a>
+            </li>
+            <li>
+              <a href="#0">Events</a>
+            </li>
+            <li>
+              <a href="#0">Products</a>
+            </li>
+            <li>
+              <a href="#0">Support</a>
+            </li>
+          </ul>
+
+          <div className="menu" onClick={toggleHandler}>
+            <img src={toggle ? `${close}` : `${menu}`} alt="menu" />
+          </div>
+        </div>
+      </nav>
+
+      <main>
+        <div className="container">
+          <h1>Immersive experiences that deliver</h1>
+        </div>
+      </main>
+    </header>
+  );
+};
+
+export default Header;
